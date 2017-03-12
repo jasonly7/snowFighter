c1706bc21c7f9f4987fe282a56b665b2a02d2bdc
Jason Ly <Jason Ly>
2017-03-12 17:18:52 -0400

    first commit

---
+++ SnowboardFighting/Assets/Snowboarder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snowboarder : MonoBehaviour {
+    float x;
+
+	// Use this for initialization
+	void Start () {
+        x = Input.mousePosition.x;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        x = Input.mousePosition.x - x;
+        
+        transform.Rotate(Vector3.up * x );
+	}
+}
