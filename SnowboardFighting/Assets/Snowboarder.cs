a5c68af127b37529f6a630ba78b5c181eaca5a19
k o <k o>
2017-03-13 20:28:13 -0400

    Shoot.cs

--- SnowboardFighting/Assets/Snowboarder.cs
+++ SnowboardFighting/Assets/Snowboarder.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Snowboarder : MonoBehaviour {
     float x;
-    private int turnSpeed = 200;
+    private int sensitivity = 10;
 	// Use this for initialization
 	void Start () {
         x = Input.mousePosition.x;
@@ -22,24 +22,27 @@ public class Snowboarder : MonoBehaviour {
     // Update is called once per frame
     void Update () {
       
-       
+       // Pressing 'A' will move to the left
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * Time.deltaTime*5);
           //  transform.Rotate(Vector3.up * 1 );
         }
+        // Pressing 'D' will move to the right
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * Time.deltaTime*5);
             //transform.Rotate(Vector3.up * -1 );
         }
 
-        if (Input.GetAxis("Mouse X") != 0)
+       if (Input.GetAxis("Mouse X") != 0)
         {
             //transform.eulerAngles.y += turnSpeed * Time.deltaTime;
-            var x = Input.GetAxis("Mouse X") * Time.deltaTime * turnSpeed;      //The above rotates this object at the speed of "speed" degrees per second in the direction pressed.
+            var x = Input.GetAxis("Mouse X") * sensitivity;      //The above rotates this object at the speed of "speed" degrees per second in the direction pressed.
             transform.eulerAngles += new Vector3(0, x, 0);
         }
+        
+
         /*  x = Input.mousePosition.x - x;
 
           transform.Rotate(Vector3.up * Time.deltaTime * x);*/
